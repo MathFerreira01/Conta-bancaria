3db63470dc6ce95729dec928eae2b35204af8e57
MathFerreira01 <ferreiramatheus124@gmail.com>
2022-01-17 15:03:41 -0300

    Updates

---
+++ precache-manifest.a589622c91d0e179ca4a99be895507d3.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7e29809eb49f356ac928f6bbd88ee8b",
+    "url": "/Conta-bancaria/index.html"
+  },
+  {
+    "revision": "80abf3a74d8e93819739",
+    "url": "/Conta-bancaria/static/js/2.c2eef6a1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Conta-bancaria/static/js/2.c2eef6a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "530fce16b6f1cb1192db",
+    "url": "/Conta-bancaria/static/js/main.263bb130.chunk.js"
+  },
+  {
+    "revision": "a7e5c8359b103d8fcb42",
+    "url": "/Conta-bancaria/static/js/runtime-main.bcb0f7bc.js"
+  },
+  {
+    "revision": "44dbaa78fb2ed240792c9e7a424b0ea0",
+    "url": "/Conta-bancaria/static/media/alimentacao.44dbaa78.svg"
+  },
+  {
+    "revision": "f3a2bff49642ca68b3841fde422295cb",
+    "url": "/Conta-bancaria/static/media/bank_logo.f3a2bff4.svg"
+  },
+  {
+    "revision": "b97c300bed11302a8687c16e07aa89e5",
+    "url": "/Conta-bancaria/static/media/dinheiro.b97c300b.svg"
+  },
+  {
+    "revision": "acd41ceb3366a71c61b7ed3d5df83682",
+    "url": "/Conta-bancaria/static/media/olho.acd41ceb.svg"
+  },
+  {
+    "revision": "17e95f3cc45dc23ffda7f4d29dc137df",
+    "url": "/Conta-bancaria/static/media/outros.17e95f3c.svg"
+  },
+  {
+    "revision": "134d853a95b2b30676da2c63cda2eeaf",
+    "url": "/Conta-bancaria/static/media/privado.134d853a.svg"
+  },
+  {
+    "revision": "605ef9ca7e70a1bb568ffcec443d05f6",
+    "url": "/Conta-bancaria/static/media/saude.605ef9ca.svg"
+  },
+  {
+    "revision": "0981f5bfd5f197c52e678c3c0616cd81",
+    "url": "/Conta-bancaria/static/media/themeOff.0981f5bf.svg"
+  },
+  {
+    "revision": "75ea28146734a5ea6593dab45d272a90",
+    "url": "/Conta-bancaria/static/media/themeOn.75ea2814.svg"
+  },
+  {
+    "revision": "15a4ed7310630a72e21a8054897c3a51",
+    "url": "/Conta-bancaria/static/media/transporte.15a4ed73.svg"
+  },
+  {
+    "revision": "59035811601fe0a607827eb6408e14ef",
+    "url": "/Conta-bancaria/static/media/utilidades.59035811.svg"
+  }
+]);
